cb3e2995c67baa725c058b038e232c229da81f89
LunaBOY65 <mooyong310103@gmail.com>
2026-04-01 13:15:52 +0700

    add Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:seeforyou_app/firebase_options.dart';
 import 'package:seeforyou_app/screens/root/root_screen.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
